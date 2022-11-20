42dd937cf0a0bb88d86cae9d12aa63cc43c6a472
Chase Newman <chasejn01@gmail.com>
2022-11-20 09:25:59 -0500

    add bitcoin api

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Connected");
+fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        console.log(data.bpi.USD.rate)
+        document.getElementById("price").innerHTML = data.bpi.USD.rate
+    });
