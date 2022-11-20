55faab1dbe10cf8f9e870682f152075933b187e8
Chase Newman <chasejn01@gmail.com>
2022-11-20 10:02:36 -0500

    add additional coin apis

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
         document.getElementById("price").innerHTML = data.bpi.USD.rate
     });
 
-fetch("https://tes-db-96d5b-default-rtdb.firebaseio.com/Questions.json")
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=ethereum")
     .then(response => response.json())
     .then(data => console.log(data));
+
+fetch("https://rest.coinapi.io/v1/exchangerate/BTC?apikey=82803CED-DC47-46DE-AB2D-8B4C86582F02")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+// fetch("https://tes-db-96d5b-default-rtdb.firebaseio.com/Questions.json")
+//     .then(response => response.json())
+//     .then(data => console.log(data));
