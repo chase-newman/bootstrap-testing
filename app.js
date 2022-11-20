d0eb71e07386edc3a4e67145cfe086c461925b30
Chase Newman <chasejn01@gmail.com>
2022-11-20 09:36:47 -0500

    connect firebase rest API

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
         console.log(data.bpi.USD.rate)
         document.getElementById("price").innerHTML = data.bpi.USD.rate
     });
+
+fetch("https://tes-db-96d5b-default-rtdb.firebaseio.com/Questions.json")
+    .then(response => response.json())
+    .then(data => console.log(data));
